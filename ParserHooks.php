3d042b85d7b85de8c3a27e8debcc6ab0f59c6b4e
jeroendedauw <jeroendedauw@gmail.com>
2013-02-10 03:41:59 +0100

    added entrypoint base

---
+++ ParserHooks.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Initialization file for the ParserHooks MediaWiki extension.
+ *
+ * Documentation:	 		https://www.mediawiki.org/wiki/Extension:ParserHooks
+ * Support					https://www.mediawiki.org/wiki/Extension_talk:ParserHooks
+ * Source code:				https://gerrit.wikimedia.org/r/gitweb?p=mediawiki/extensions/ParserHooks.git
+ *
+ * @file
+ * @ingroup ParserHooks
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+/**
+ * This documentation group collects source code files belonging to ParserHooks.
+ *
+ * @defgroup ParserHooks ParserHooks
+ */
+
+/**
+ * Tests part of the ParserHooks extension.
+ *
+ * @defgroup ParserHooksTests ParserHooksTest
+ * @ingroup ParserHooks
+ * @ingroup Test
+ */
+
+define( 'ParserHooks_VERSION', '0.1 alpha' );
+
+// @codeCoverageIgnoreStart
+call_user_func( function() {
+	// TODO
+} );
+// @codeCoverageIgnoreEnd
