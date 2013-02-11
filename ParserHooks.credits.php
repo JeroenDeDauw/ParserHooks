ab725fc37332189be037fc0bba5cb4b8fe647a42
jeroendedauw <jeroendedauw@gmail.com>
2013-02-11 01:21:30 +0100

    added credits

---
+++ ParserHooks.credits.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Credits file for the ParserHooks MediaWiki extension.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup ParserHooks
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+return array(
+	'path' => __FILE__,
+	'name' => 'ParserHooks',
+	'version' => ParserHooks_VERSION,
+	'author' => array(
+		'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
+	),
+	'url' => 'https://www.mediawiki.org/wiki/Extension:ParserHooks',
+	'descriptionmsg' => 'parserhooks-desc'
+);
