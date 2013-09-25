d484d52b06f6078347ccbe953fef4bc2b6378343
jeroendedauw <jeroendedauw@gmail.com>
2013-09-25 21:35:09 +0200

    Add to HookRegistrantTest
    Change-Id: Iaa701b7138023215f2da7e2eb4e99475c9358013

--- tests/phpunit/HookRegistrantTest.php
+++ tests/phpunit/HookRegistrantTest.php
@@ -15,27 +15,98 @@ use ParserHooks\HookRegistrant;
  */
 class HookRegistrantTest extends \PHPUnit_Framework_TestCase {
 
-	public function testRegisterFunction() {
+	// TODO: figure out how to assert this properly
+	public function namesProvider() {
+		return array(
+			array( array( 'foo' ) ),
+//			array( array( 'foo', 'bar' ) ),
+//			array( array( 'foo', 'bar', 'baz', 'bah' ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider namesProvider
+	 */
+	public function testRegisterFunction( array $names ) {
+		$parser = $this->newMockParserForFunction( $names );
+		$registrant = new HookRegistrant( $parser );
+
+		$registrant->registerFunction( $this->newMockRunner( $names, 'ParserHooks\FunctionRunner' ) );
+	}
+
+	/**
+	 * @dataProvider namesProvider
+	 */
+	public function testRegisterHook( array $names ) {
+		$parser = $this->newMockParserForHook( $names );
+		$registrant = new HookRegistrant( $parser );
+
+		$registrant->registerHook( $this->newMockRunner( $names, 'ParserHooks\HookRunner' ) );
+	}
+
+	/**
+	 * @dataProvider namesProvider
+	 */
+	public function testRegisterFunctionHandler( array $names ) {
+		$parser = $this->newMockParserForFunction( $names );
+		$registrant = new HookRegistrant( $parser );
+
+		$registrant->registerFunctionHandler(
+			new HookDefinition( $names ),
+			$this->getMock( 'ParserHooks\HookHandler' )
+		);
+	}
+
+	/**
+	 * @dataProvider namesProvider
+	 */
+	public function testRegisterHookHandler( array $names ) {
+		$parser = $this->newMockParserForHook( $names );
+		$registrant = new HookRegistrant( $parser );
+
+		$registrant->registerHookHandler(
+			new HookDefinition( $names ),
+			$this->getMock( 'ParserHooks\HookHandler' )
+		);
+	}
+
+	protected function newMockParserForFunction( array $names ) {
 		$parser = $this->getMock( 'Parser' );
 
-		$parser->expects( $this->exactly( 3 ) )
+		$parser->expects( $this->exactly( count( $names ) ) )
 			->method( 'setFunctionHook' )
-			->with( $this->equalTo( 'foo' ) );
+			->with(
+				$this->equalTo( $names[0] ),
+				$this->isType( 'callable' )
+			);
 
-		$definition = new HookDefinition(
-			array( 'foo', 'foo', 'foo' )
-		);
+		return $parser;
+	}
 
-		$runner = $this->getMockBuilder( 'ParserHooks\FunctionRunner' )
+	protected function newMockParserForHook( array $names ) {
+		$parser = $this->getMock( 'Parser' );
+
+		$parser->expects( $this->exactly( count( $names ) ) )
+			->method( 'setHook' )
+			->with(
+				$this->equalTo( $names[0] ),
+				$this->isType( 'callable' )
+			);
+
+		return $parser;
+	}
+
+	protected function newMockRunner( array $names, $runnerClass ) {
+		$definition = new HookDefinition( $names );
+
+		$runner = $this->getMockBuilder( $runnerClass )
 			->disableOriginalConstructor()->getMock();
 
 		$runner->expects( $this->once() )
 			->method( 'getDefinition' )
 			->will( $this->returnValue( $definition ) );
 
-		$registrant = new HookRegistrant( $parser );
-
-		$registrant->registerFunction( $runner );
+		return $runner;
 	}
 
 }
